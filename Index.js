cc096acfed7761c41289494a73096411359118c4
Zubair <zbrlohar@gmail.com>
2024-08-20 17:08:02 +0500

    vercel update

--- Index.js
+++ Index.js
@@ -152,7 +152,7 @@ const userRoutes = router.get("/", async (req, res) => {
 
         console.log("Connected to MongoDB");
         const users = await newUser.find();
-        res.json(users);
+        data = res.json(users);
         
     } catch (err) {
       console.error(err.message);
